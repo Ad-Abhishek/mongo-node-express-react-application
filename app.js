f7442a63830525b5f1b91f7ed51afc9bb3753c54
Abhishek Adhikari <abhishek.adhikari276@gmail.com>
2024-08-09 22:30:58 +0545

    use MongoDB for CRUD operation

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 const productRouter = require('./productRouter');
 
 dotenv.config();
@@ -9,6 +10,15 @@ const app = express();
 
 app.use(cors());
 app.use(express.json()); //To parse JSON request bodies
+
+// Connect to MongoDB
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch((err) => console.error('Could not connect to MongoDB:', err));
 
 // Use the product router
 app.use('', productRouter);
--- productController.js
+++ productController.js
@@ -1,63 +1,71 @@
-let products = [
-  {
-    id: 1,
-    name: 'iPhone 15 pro max',
-    price: 1000,
-  },
-];
+let Product = require('./productModel');
 
 // Get all produts
-const getAllProducts = (req, res) => {
-  res.json(products);
+const getAllProducts = async (req, res) => {
+  try {
+    const products = await Product.find();
+    res.json(products);
+  } catch (err) {
+    res.status(500).json({
+      message: err.message,
+    });
+  }
 };
 
 // Get a product by ID
-const getProductById = (req, res) => {
-  const product = products.find((p) => p.id === parseInt(req.params.id));
-  if (!product)
-    return res.status(404).json({
-      message: 'Product not found',
+const getProductById = async (req, res) => {
+  try {
+    const product = await Product.findById(req.params.id);
+    if (!product)
+      return res.status(404).json({
+        message: 'Product not found',
+      });
+    res.json(product);
+  } catch (error) {
+    res.status(500).json({
+      message: err.message,
     });
-  res.json(product);
+  }
 };
 
 // Create a new produt
-const createProduct = (req, res) => {
-  const newProduct = {
-    id: products.length + 1,
+const createProduct = async (req, res) => {
+  const newProduct = new Product({
     name: req.body.name,
     price: req.body.price,
-  };
-  products.push(newProduct);
-  res.status(201).json(newProduct);
+  });
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(201).json(savedProduct);
+  } catch (error) {
+    res.status(400).json({ message: err.message });
+  }
 };
 
 // Update a product
-const updateProduct = (req, res) => {
-  const product = products.find((p) => p.id === parseInt(req.params.id));
-  if (!product)
-    return res.status(404).json({
-      message: 'Product Not found',
+const updateProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
     });
-
-  product.name = req.body.name || product.name;
-  product.price = req.body.price || product.price;
-
-  res.json(product);
+    res.status(200).json(product);
+  } catch (error) {
+    res.status(400).json({ message: error.message });
+  }
 };
 
 // Delete a product
-const deleteProduct = (req, res) => {
-  const productIndex = products.findIndex(
-    (p) => p.id === parseInt(req.params.id)
-  );
-  if (productIndex === -1)
-    return res.status(404).json({
-      message: 'Product not found',
-    });
+const deleteProduct = async (req, res) => {
+  try {
+    const product = await Product.findByIdAndDelete(req.params.id);
+    if (!product) return res.status(404).json({ message: 'Product not found' });
 
-  products.splice(productIndex, 1);
-  res.status(204).send();
+    res.status(200).json({
+      message: 'Product Deleted Successfully!',
+    });
+  } catch (err) {
+    res.status(500).json({ message: err.message });
+  }
 };
 
 module.exports = {
